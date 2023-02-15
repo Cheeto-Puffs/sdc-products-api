f83e9eae891410659fb386d524dcfcad1b7f86f1
Kevin Le <iamkevinvle@gmail.com>
2023-02-15 15:09:52 -0800

    load data into DB using COPY

--- Schema.sql
+++ Schema.sql
@@ -12,13 +12,11 @@ description VARCHAR(1000),
 category VARCHAR(50),
 default_price INTEGER
 );
-
+ALTER TABLE products ADD CONSTRAINT products_pkey PRIMARY KEY (product_id);
 COPY products (product_id, name, slogan, description, category, default_price)
 FROM '/Users/kevinle/Downloads/products-data/product.csv'
 DELIMITER ','
 CSV HEADER;
-
-ALTER TABLE products ADD CONSTRAINT products_pkey PRIMARY KEY (product_id);
 
 CREATE TABLE related (
 id BIGSERIAL,
@@ -26,6 +24,10 @@ product_id INTEGER,
 related_id INTEGER
 );
 ALTER TABLE related ADD CONSTRAINT related_pkey PRIMARY KEY (id);
+COPY related (id, product_id, related_id)
+FROM '/Users/kevinle/Downloads/products-data/related.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE features (
 feature_id BIGSERIAL,
@@ -33,27 +35,37 @@ feature VARCHAR(1000),
 value VARCHAR(1000),
 product_id INTEGER
 );
-
 ALTER TABLE features ADD CONSTRAINT features_pkey PRIMARY KEY (feature_id);
-
+COPY features (feature_id, product_id, feature, value)
+FROM '/Users/kevinle/Downloads/products-data/features.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE styles (
 style_id BIGSERIAL,
 name VARCHAR(255),
-original_price INTEGER,
-sale_price INTEGER,
+original_price VARCHAR(50),
+sale_price VARCHAR(50),
 default_style BOOLEAN,
 product_id INTEGER
 );
 ALTER TABLE styles ADD CONSTRAINT styles_pkey PRIMARY KEY (style_id);
+COPY styles (style_id, product_id, name, sale_price, original_price, default_style)
+FROM '/Users/kevinle/Downloads/products-data/styles.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE photos (
 photo_id BIGSERIAL,
-thumbnail_url VARCHAR(1000),
-url VARCHAR(1000),
+thumbnail_url TEXT,
+url TEXT,
 style_id INTEGER
 );
 ALTER TABLE photos ADD CONSTRAINT photos_pkey PRIMARY KEY (photo_id);
+COPY photos (photo_id, style_id, url, thumbnail_url)
+FROM '/Users/kevinle/Downloads/products-data/photos.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE skus (
 sku_id BIGSERIAL,
@@ -62,6 +74,10 @@ size VARCHAR(25),
 style_id INTEGER
 );
 ALTER TABLE skus ADD CONSTRAINT skus_pkey PRIMARY KEY (sku_id);
+COPY skus (sku_id, style_id, size, quantity)
+FROM '/Users/kevinle/Downloads/products-data/skus.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE cart (
 cart_id BIGSERIAL,
@@ -72,7 +88,7 @@ ALTER TABLE cart ADD CONSTRAINT cart_pkey PRIMARY KEY (cart_id);
 
 -- add foreign keys
 ALTER TABLE related ADD CONSTRAINT related_product_id_fkey FOREIGN KEY (product_id) REFERENCES products(product_id);
-ALTER TABLE related ADD CONSTRAINT related_related_id_fkey FOREIGN KEY (related_id) REFERENCES products(product_id);
+ALTER TABLE related ADD CONSTRAINT related_related_id_fkey FOREIGN KEY (product_id) REFERENCES products(product_id);
 ALTER TABLE features ADD CONSTRAINT features_product_id_fkey FOREIGN KEY (product_id) REFERENCES products(product_id);
 ALTER TABLE styles ADD CONSTRAINT styles_product_id_fkey FOREIGN KEY (product_id) REFERENCES products(product_id);
 ALTER TABLE photos ADD CONSTRAINT photos_style_id_fkey FOREIGN KEY (style_id) REFERENCES styles(style_id);
