e771469128b9459818a25acb8c2e1cd4c1cc657b
Kevin Le <iamkevinvle@gmail.com>
2023-02-21 15:08:00 -0800

    easily switch between cached and uncached versions of controller

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -67,3 +67,45 @@ module.exports = {
     }
   }
 }
+
+// NO CACHE
+// const model = require('../models/products.js')
+// module.exports = {
+
+//   getProducts: async (req, res) => {
+//     try {
+//       const products = await model.getAllProducts()
+//       res.status(200).send(products)
+//     } catch(err) {
+//       res.status(400).send({ message: 'Error requesting styles', error: {err: err, message: err.message}})
+//     }
+//   },
+
+//   getProductById: async (req, res) => {
+//     try {
+//       const productInfo = await model.getProduct(req.params.product_id)
+//       res.status(200).send(productInfo)
+//     } catch (err) {
+//       console.log(err)
+//       res.status(400).send({ message: 'Error requesting product by id', error: {err: err, message: err.message}})
+//     }
+//   },
+
+//   getStylesById: async (req, res) => {
+//     try {
+//       const styles = await model.getProductStyles(req.params.product_id);
+//       res.status(200).send(styles)
+//     } catch (err) {
+//       res.status(400).send({ message: 'Error requesting styles by id', error: {err: err, message: err.message}})
+//     }
+//   },
+
+//   getRelatedProductsById: async (req, res) => {
+//     try {
+//       const relatedProducts = await model.getRelated(req.params.product_id)
+//       res.status(200).send(relatedProducts)
+//     } catch (err) {
+//       res.status(400).send({ message: 'Error requesting related products by id', error: {err: err, message: err.message}})
+//     }
+//   }
+// }
