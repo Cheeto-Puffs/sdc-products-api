9964e319adfa73be98e43c4fa30015615aeedba1
Kevin Le <iamkevinvle@gmail.com>
2023-02-20 20:35:36 -0800

    implement redis caching layer

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,4 +1,6 @@
 const model = require('../models/products.js')
+const redis = require('../cache/redis.js')
+
 module.exports = {
 
   getProducts: async (req, res) => {
@@ -12,8 +14,21 @@ module.exports = {
 
   getProductById: async (req, res) => {
     try {
+      // query the cache first if we find a result, return that result
+      let cacheEntry = await redis.get(`product:${req.params.product_id}`)
+
+      if (cacheEntry) {
+        cacheEntry = JSON.parse(cacheEntry)
+        res.status(200).send({ ... cacheEntry, 'source' : 'cache' })
+        return
+      }
+      // otherwise we query the DB
       const productInfo = await model.getProduct(req.params.product_id)
-      res.status(200).send(productInfo)
+
+      // store results in the cache before we send back to the client
+      redis.set(`product:${req.params.product_id}`, JSON.stringify(productInfo))
+
+      res.status(200).send({... productInfo, 'source': 'DB'})
     } catch (err) {
       console.log(err)
       res.status(400).send({ message: 'Error requesting product by id', error: {err: err, message: err.message}})
@@ -25,7 +40,7 @@ module.exports = {
       const styles = await model.getProductStyles(req.params.product_id);
       res.status(200).send(styles)
     } catch (err) {
-      res.status(400).send({ message: 'Error requesting styles by id', error: {err: err, message: err.message}})
+      res.status(404).send({ message: 'Error requesting styles by id', error: {err: err, message: err.message}})
     }
   },
 
