1d61939d16265a5968f919aae961f7b0c9e82f56
Kevin Le <iamkevinvle@gmail.com>
2023-02-22 20:48:17 -0800

    listen for loader_io

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -17,14 +17,16 @@ module.exports = {
       let key = `product:${req.params.product_id}`
       let cacheEntry = await cache.getFromCache(key);
       if (cacheEntry) {
-        res.status(200).send({ ... cacheEntry, 'source' : 'cache' })
+        // res.status(200).send({ ... cacheEntry, 'source' : 'cache' })
+        res.status(200).send(cacheEntry)
         return
       }
 
       // cache miss
       const productInfo = await model.getProduct(req.params.product_id)
       cache.setInCache(key, JSON.stringify(productInfo))
-      res.status(200).send({... productInfo, 'source': 'DB'})
+      // res.status(200).send({... productInfo, 'source': 'DB'})
+      res.status(200).send(productInfo)
     } catch (err) {
       console.log(err)
       res.status(400).send({ message: 'Error requesting product by id', error: {err: err, message: err.message}})
@@ -37,14 +39,16 @@ module.exports = {
       let key = `product:${req.params.product_id}:style`
       const cacheEntry = await cache.getFromCache(key)
       if (cacheEntry) {
-        res.status(200).send({...cacheEntry, 'source': 'cache'})
+        // res.status(200).send({...cacheEntry, 'source': 'cache'})
+        res.status(200).send(cacheEntry)
         return
       }
 
       // cache miss
       const styles = await model.getProductStyles(req.params.product_id);
       cache.setInCache(key, JSON.stringify(styles));
-      res.status(200).send({...styles, 'source': 'DB'})
+      // res.status(200).send({...styles, 'source': 'DB'})
+      res.status(200).send(styles)
     } catch (err) {
       res.status(404).send({ message: 'Error requesting styles by id', error: {err: err, message: err.message}})
     }
@@ -55,13 +59,15 @@ module.exports = {
       let key = `product:${req.params.product_id}:related`
       const cacheEntry = await cache.getFromCache(key)
       if (cacheEntry) {
-        res.status(200).send([...cacheEntry, 'cache'])
+        // res.status(200).send([...cacheEntry, 'cache'])
+        res.status(200).send(cacheEntry)
         return
       }
 
       const relatedProducts = await model.getRelated(req.params.product_id)
       cache.setInCache(key, JSON.stringify(relatedProducts));
-      res.status(200).send([...relatedProducts, 'DB'])
+      // res.status(200).send([...relatedProducts, 'DB'])
+      res.status(200).send(relatedProducts)
     } catch (err) {
       res.status(400).send({ message: 'Error requesting related products by id', error: {err: err, message: err.message}})
     }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./routes.js')
@@ -8,6 +9,12 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/', router);
+
+// LOADER_IO
+const filePath = process.env.LOADER_IO;
+Router.get(`/${filePath}`, (req, res) => {
+  res.sendFile(path.join(__dirname, filePath, '.txt'))
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`)
